778b05db92696824090261a2479796ea560265db
kmpdangerous-netizen <kmpdangerous@gmail.com>
2026-03-24 18:53:19 -0600

    Updated sensor_logger.cpp

--- Code/src/sensor_logger.cpp
+++ Code/src/sensor_logger.cpp
@@ -29,5 +29,55 @@ TASK_RETURN_CODE_T initializeLogger(SENSOR_TASK* sensorTasks, CAMERA_TASK* camer
 
 TASK_RETURN_CODE_T logNewData() {
     TASK_RETURN_CODE_T retVal = TASK_EXECUTION_INCOMPLETE;
+
+    unsigned position, fileNumber;
+    bool writeHeader = false;
+    
+    FsFile positionFile = SD_CARD.open(PATH_TO_LOG_POS, FILE_READ);
+    positionFile.seek(0);
+    fileNumber = positionFile.parseInt();
+    position = positionFile.parseInt();
+    positionFile.close();
+    
+    position++;
+    if(fileNumber == 0 || position > CSV_LENGTH){
+        fileNumber++;
+        position = 1;
+        writeHeader = true;
+    }
+    
+    positionFile = SD_CARD.open(PATH_TO_LOG_POS, O_RDWR);
+    positionFile.println(fileNumber);
+    positionFile.println(position);
+    positionFile.close();
+    
+    FsFile CSV = SD_CARD.open(PATH_TO_LOG + fileNumber + ".csv", FILE_WRITE);
+
+    if(writeHeader) {
+        for(uint8_t sensor = 0; sensor < NUM_SENSOR_TASKS; sensor++){
+            for(uint8_t data = 0; data < sensorTasks[sensor]->numDataTypes; data++){
+                CSV.print(sensorTasks[sensor]->dataNames[data]);
+                if(sensor != NUM_SENSOR_TASKS - 1 || data != sensorTasks[sensor]->numDataTypes - 1){
+                    CSV.print(',');
+                }
+            }
+        }
+        CSV.println("Photo #");
+    }
+
+    for(uint8_t sensor = 0; sensor < NUM_SENSOR_TASKS; sensor++){
+        for(uint8_t data = 0; data < sensorTasks[sensor]->numDataTypes; data++){
+            CSV.print(sensorTasks[sensor]->dataCSV[data]);
+            CSV.print(',');
+        }
+    }
+
+    if(cameraTaskPtr->lastPhotoTaken != -1) {
+        CSV.print(cameraTaskPtr->lastPhotoTaken);
+        cameraTaskPtr->lastPhotoTaken = -1;
+    }
+
+    CSV.println();
+
     return retVal;
 }
